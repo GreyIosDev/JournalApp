e2004d959b107ddc7abf1a1861d81f87603b9564
NNADI HENRY UGONNA <greyios845@gmail.com>
2023-05-24 01:17:05 +0200

    Initial Commit

---
+++ JournalApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  JournalApp
+//
+//  Created by Grey  on 24.05.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JournalApp/JournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournalAppApp.swift
+//  JournalApp
+//
+//  Created by Grey  on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct JournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
